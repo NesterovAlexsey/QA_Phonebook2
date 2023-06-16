c17d52339adba6f39e4e1fd0ed40ef5a272ba51b
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-06-16 17:21:22 +0200

    finish with create account test, start with login test

--- src/test/java/com/phonebook/tests/CreateAccountTests.java
+++ src/test/java/com/phonebook/tests/CreateAccountTests.java
@@ -1,9 +1,12 @@
 package com.phonebook.tests;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class CreateAccountTests extends TestBase {
+
   //precondition: user should be logged out
   @BeforeMethod
   public void ensurePrecondition() {
@@ -12,12 +15,29 @@ public class CreateAccountTests extends TestBase {
     }
   }
 
+  @Test
+  public void newUserRegistrationPositiveTest() {
+    //click on Login link
+    click((By.cssSelector("[href='/login']")));
+    //enter email field
+    type();
+    //enter password
+    driver.findElement(By.cssSelector("[placeholder='Password']")).click();
+    driver.findElement(By.cssSelector("[placeholder='Password']")).clear();
+    driver.findElement(By.cssSelector("[placeholder='Password']")).sendKeys("Word212345$");
+    // click on registration button
+    driver.findElement(By.name("registration")).click();
+    //assert Sign out button is displayed
+    Assert.assertTrue(isElementPresent(By.xpath("//button[.='Sign Out']")));
+  }
 
+  public void type() {
+    driver.findElement(By.cssSelector("[placeholder='Email']")).click();
+    driver.findElement(By.cssSelector("[placeholder='Email']")).clear();
+    driver.findElement(By.cssSelector("[placeholder='Email']")).sendKeys("alex2@gmail.com");
+  }
 
-  //click on Login link
-  //enter email field
-  //enter password
-  // click on login button
-  //assert Sign out button is displayed
-
+  public void click(By locator) {
+    driver.findElement(locator);
+  }
 }
